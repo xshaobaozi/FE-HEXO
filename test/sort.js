28e1852c974996e66c900c940b7bd8e2b295f8d6
xshaobaozi <xshaobaozi@hotmail.com>
2020-03-04 17:58:51 +0800

    mod sort

--- test/sort.js
+++ test/sort.js
@@ -1,31 +1,24 @@
 /*
  * @Date: 2020-03-04 11:14:13
  * @LastEditors: xshaobaozi
- * @LastEditTime: 2020-03-04 14:03:54
+ * @LastEditTime: 2020-03-04 17:13:08
  */
 
-testLog(selectionSort([3, 2, 1, 2, 12, 45, 13, 76, 43, 21, 1, 0, 54, 34]))
+shellSort([3, 2, 1, 2, 12, 45, 13, 76, 43, 21, 1, 0, 54, 34])
 
 
-function selectionSort(arr) {
+function shellSort(arr) {
     var len = arr.length
-    for (var i = 0; i < len - 1; i++) {
-        var temp = null, minIndex = i;
-        for (var j = i + 1; j < len; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j
-            }
-        }
-        temp = arr[i]
-        arr[i] = arr[minIndex]
-        arr[minIndex] = temp
+    var temp;
+    var gap = 1;
+    while(gap < len / 3) {
+        gap = gap * 3 + 1
     }
-    return arr
 }
 
 
-function testLog(arr) {
-    console.log('排序结果:')
+function testLog(i, arr) {
+    console.log('排序结果:', i)
     console.log(arr)
 }
 
